316239f28200acecac0d0e8c8aa34d41d4e17c5d
khbesbes <5786@holbertonstudents.com>
2023-02-10 10:02:06 +0100

    fix pep8

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for digt1 in range(9):
-    for digt2 in range (digt1 + 1, 10):
+    for digt2 in range(digt1 + 1, 10):
         if digt1 == 8 and digt2 == 9:
             print(("{}{}").format(digt1, digt2))
         else:
             print("{}{}".format(digt1, digt2), end=", ")
-            
