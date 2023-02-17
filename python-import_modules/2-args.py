48eb86cc7a93508686ca19d5c5479081d25affec
khbesbes <5786@holbertonstudents.com>
2023-02-17 01:03:11 +0100

    fix pep 8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ =="__main__":
-    if len(sys.argv) -1 == 1:
+    if len(sys.argv) - 1 == 1:
         print("1 argument:")
     elif len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(sys.argv) -  1))
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i,sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
