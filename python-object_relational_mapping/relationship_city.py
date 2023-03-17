7b1152526ebcf097711cded3f4b0872d3e127b3b
khbesbes <5786@holbertonstudents.com>
2023-03-17 23:21:24 +0100

    relationship_city.py

---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python3
+""" class Model """
+from sqlalchemy import Integer, String, Column, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """
+        class Model city
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    
