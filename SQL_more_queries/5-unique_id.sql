c1dd2e3db9830c9c37175562c5aa6dfb0b5f426d
khbesbes <5786@holbertonstudents.com>
2023-03-17 10:26:02 +0100

    5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
--- Create database and table
-CREATE DATABASE if not exists hbtn_0d_usa;
-CREATE TABLE if not exists hbtn_0d_usa.states (
-    id int SERIAL DEFAULT VALUE primary key,
-    name VARCHAR(256) not null
-    );
+-- Create table
+create table if not exists unique_id (
+    id int unique DEFAULT 1,
+    name VARCHAR(256)
+);
